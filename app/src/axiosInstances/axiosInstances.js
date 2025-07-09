22a93322834b7d361d718b079586ca83d802fc8a
porlanagustin <porlan.agustin@gmail.com>
2025-07-08 22:50:00 -0300

    improvement in frontend

--- app/src/axiosInstances/axiosInstances.js
+++ app/src/axiosInstances/axiosInstances.js
@@ -1,20 +1,7 @@
 import axios from "axios";
 
-/*
 export const authApi = axios.create({
-  baseURL: `http://localhost:${
-    import.meta.env.VITE_BACKEND_PORT || 8000
-  }/v1/auth`,
-  headers: {
-    "Content-Type": "application/json",
-  },
-});
-*/
-
-export const authApi = axios.create({
-  baseURL:
-    `${import.meta.env.VITE_BACKEND_RAILWAY_PUBLIC}/v1` ||
-    "backend-asuser-production.up.railway.app/v1",
+  baseURL: "https://backend-asuser-production.up.railway.app/v1/auth",
   headers: {
     "Content-Type": "application/json",
   },
